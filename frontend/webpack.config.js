7f543763bf9a080ba909aa2d3f58c416bc6c79d6
Igor Brinker <igorbrinker@gmail.com>
2021-03-22 19:59:12 -0300

    First commit

---
+++ frontend/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "./static/")
+    }
+}
